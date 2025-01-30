dbb6f6d4d17a6b5d7fc29b52ed4b6b87b931543b
Geovanna Souza Santos <55329011809@>
2025-01-30 10:08:59 -0300

    segundaaula

---
+++ segundaaula.py
@@ -0,0 +1,17 @@
+class Cachorro:
+	def fazer_som(self):
+		print("Auau")
+
+class Gato:
+	def fazer_som(self):
+		print("Miau")
+
+cachorro = Cachorro()
+gato = Gato()
+
+cachorro.fazer_som()
+gato.fazer_som()
+
+
+        
+
